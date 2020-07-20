cbcab1a069abd2bdac568358994ed11b05e96f3c
Dima <dg646726@gmail.com>
2020-07-20 11:39:25 +0300

    Add project files.

---
+++ Урок№28_ООП/Source.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct InfoAboutCar
+{
+	string name;
+	int graduationYear;
+	float engineCapacity;
+	float price;
+};
+
+class CarDealership
+{
+private:
+	vector<InfoAboutCar> cars;
+
+public:
+};
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
